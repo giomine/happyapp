d6bed6d7e9ee7e8e5674f8e41f7dbf83fbb82a3a
giomine <giomineo@gmail.com>
2021-04-09 13:56:58 +0100

    Fix bootstrap navbar hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+require('bootstrap');
 
 function Navbar() {
 
@@ -7,9 +8,14 @@ function Navbar() {
         <nav className="navbar navbar-dark bg-info navbar-expand-lg">
         <div className="container">
                 <Link to="/" className="navbar-brand">HappyApp</Link>
-                <div className="collapse navbar-collapse">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
+                        <li className="nav-item">
                             <Link to="/" className="nav-link">Daily Log</Link>
                         </li>
                     </ul>
