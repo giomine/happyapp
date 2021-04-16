bb57f2ba0dffd0977a0c8751f22d34df0bee0148
giomine <giomineo@gmail.com>
2021-04-16 11:36:34 +0100

    Add props to CreateLogContainer styled-component + change color scheme to blue

--- src/components/styledcomponents/CreateLogContainer.js
+++ src/components/styledcomponents/CreateLogContainer.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export default styled.div`
     border-radius: 10px;
     margin: 0 auto;
-    background: rgba(295, 182, 60, 0.6);
+    ${'' /* background: ${props => (props.orange ? "rgba(295, 182, 60, 0.6)" : "rgba(53, 192, 214, 0.8)")}; */}
+    background: ${props => (props.paleBlue ? "rgba(53, 192, 214, 0.35)" : "rgba(53, 192, 214, 0.5)")};
+    color: rgb(30,30,30);
     padding: 10px 30px;
 
     @media(min-width: 770px) {
