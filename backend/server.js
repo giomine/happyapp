0de690bd61e12ec4f96868e05b4cea80bec142ec
giorgia-mineo <giomineo@gmail.com>
2023-02-11 17:26:10 +0000

    Reconnected paused database

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const dayRouter = require('./routes/day');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
