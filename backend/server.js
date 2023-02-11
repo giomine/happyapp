c376574ab0d3f6fefa7d2dd51303a38d516224fa
giorgia-mineo <giomineo@gmail.com>
2023-02-11 17:59:20 +0000

    Update env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
